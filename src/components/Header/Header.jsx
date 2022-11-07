6a95b75353227cb840d08d34c13f85c8b7a45b57
David Liu <liu.d2@student.unimelb.edu.au>
2022-11-07 11:53:42 +1100

    semi completed header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
 import './Header.css'
+import Resume from '../../assets/DavidLiuResume.pdf'
+import Pic from '../../assets/IMG-1379.jpg'
+import {BsLinkedin} from 'react-icons/bs'
+import {BsGithub} from 'react-icons/bs'
+import {BsFacebook} from 'react-icons/bs'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className='container header_container'>
+        <h5>Hello I'm</h5>
+        <h1>David Liu</h1>
+        <h5 className='text-light'>Fullstack Developer</h5>
+
+        <div className='cta'>
+          <a href={Resume} download className='btn'>Download Resume</a>
+          <a href='#contact' className='btn btn2'>Contact Me</a>
+        </div>
+
+        <div className='socials'>
+          <a href='https://linkedin.com/in/davidliu0115' target="_blank"><BsLinkedin/></a>
+          <a href='https://github.com/megagnarly' target="_blank"><BsGithub/></a>
+          <a href='https://facebook.com/davidliu0115' target="_blank"><BsFacebook/></a>
+        </div>
+
+        <div className='me'>
+          <img className='pic' src={Pic} alt="pic"/>
+        </div>
+        
+
+
+      </div>
+    </header>
   )
 }
 
